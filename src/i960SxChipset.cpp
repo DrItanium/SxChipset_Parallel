d2970262475e0dd0071597e2d44917421378ec49
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-12-30 13:07:36 -0800

    Undo hacks from the pure serial design

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -533,9 +533,9 @@ void loop() {
     // this is expensive and something I don't use. The infinite loop is for this purpose. It shaves off around 0.1usec in the worst case
     // and doesn't seem to impact performance in burst transactions
 
-    for (;;) {
+    //for (;;) {
         doInvocationBody<CompileInAddressDebuggingSupport>();
-    }
+    //}
 }
 
 [[noreturn]]
